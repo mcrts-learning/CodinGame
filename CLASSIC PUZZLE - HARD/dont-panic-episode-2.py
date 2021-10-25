704de26e463e0fd1327364934ea0bfd3435a85a2
Martin Courtois <martin.courtois@protonmail.com>
2021-10-25 11:56:41 +0200

    [WIP] graph reduction and bfs solver

--- CLASSIC PUZZLE - HARD/dont-panic-episode-2.py
+++ CLASSIC PUZZLE - HARD/dont-panic-episode-2.py
@@ -1,114 +1,286 @@
-import sys
-from collections import namedtuple, Counter
+from typing import DefaultDict, Iterable, MutableSet, Tuple, FrozenSet
+from dataclasses import dataclass, field
+from collections import Counter
+import heapq as hq
 
-def log(msg, *args, **kwargs):
-    print(msg, *args, file=sys.stderr, flush=True, **kwargs)
+import numpy as np
 
 WAIT = ("WAIT",)
-ELEVATOR = ("ELEVATOR",)
-BLOCK = ("BLOCK",)
-# WAIT_BLOCK = ("WAIT", "BLOCK", "WAIT", "WAIT")
-# (WAIT_BLOCK, Node(node.x - 2*node.direction, node.y, -node.direction))
+ELEVATOR = ("ELEVATOR",) + 3*WAIT
+BLOCK = ("BLOCK",) + 2*WAIT
 
-class Node(namedtuple('Node', ['x', 'y', 'direction'])):
-    pass
+@dataclass(frozen=True, order=True)
+class Node:
+    position: Tuple[int, int, int]
+    label: str = field(default='', compare=False)
 
-class State:
-    def __init__(self, height, width, time, n_clone, n_elevator, exit):
-        self.height = height
-        self.width = width
-        self.time = time
-        self.n_clone = n_clone
-        self.n_elevator = n_elevator
-        self.elevators = [[False for _ in range(width)] for _ in range(height)]
-        self.blocks = [[False for _ in range(width)] for _ in range(height)]
-        self.exit = exit
-        self.start = None
+    @property
+    def y(self):
+        return self.position[0]
 
-    def add_elevator(self, x, y):
-        self.elevators[y][x] = True
+    @property
+    def x(self):
+        return self.position[1]
 
-    def add_block(self, x, y):
-        self.blocks[y][x] = True
+    @property
+    def direction(self):
+        return self.position[2]
 
-    def set_start(self, x, y):
-        self.start = (x, y)
+@dataclass
+class NodeProperties:
+    is_elevator: bool = False
+    is_exit: bool = False
+    is_start: bool = False
+    can_walk: bool = False
+    can_block: bool = False
+    can_use_elevator: bool = False
+    can_put_elevator: bool = False
+    can_exit: bool = False
 
-    def get_adjacents(self, node):
-        adjacents = []
-        if 0 <= node.x + node.direction <= self.width:
-            adjacents.append((WAIT, Node(node.x + node.direction, node.y, node.direction)))
-        if 0 <= node.y < self.height - 1:
-            adjacents.append((ELEVATOR, Node(node.x, node.y + 1, node.direction)))
-        adjacents.append((BLOCK, Node(node.x - node.direction, node.y, -node.direction)))
-        return adjacents
+    def __repr__(self):
+        return f"""{self.__class__.__name__}({', '.join([f"{k}={v}" for k, v in self.__dict__.items() if v is True])})"""
 
-    def simulate(self, node, actions):
-        time, n_clone, n_elevator = cost(actions)
-        if time > self.time:
-            return node, "ERROR | too long"
-        elif n_clone > self.n_clone:
-            return node, "ERROR | not enough clone"
-        elif n_elevator > self.n_elevator:
-            return node, "ERROR | not enough elevator"
-        else:
-            for action in actions:
-                if action == ELEVATOR[0]:
-                    node = Node(node.x, node.y + 1, node.direction)
-                elif action == BLOCK[0]:
-                    node = Node(node.x - node.direction, node.y, -node.direction)
-                else:
-                    if 0 <= node.x + node.direction <= self.width:
-                        node = Node(node.x + node.direction, node.y, node.direction)
-                    else:
-                        return node, "ERROR | clone died"
-                if (node.x, node.y) == self.exit:
-                    return node, "OK"
-        return node, "OK"
+@dataclass(frozen=True, order=True)
+class Edge:
+    node_from: Node
+    node_to: Node
+    label: str = field(default="", compare=False)
+    sequence: Iterable[str] = field(default_factory=tuple, compare=False)
 
-def cost(actions):
-    counter = Counter(actions)
-    time = counter['WAIT'] + 4*counter['ELEVATOR'] + 4*counter['BLOCK']
-    clone = counter['ELEVATOR'] + counter['BLOCK']
-    elevator = counter['ELEVATOR']
-    return time, clone, elevator
+    @classmethod
+    def walk_edge(cls, n1, n2):
+        return cls(n1, n2, "WALK", WAIT*abs(n1.x - n2.x))
+
+    @classmethod
+    def block_edge(cls, n1, n2):
+        return cls(n1, n2, "BLOCK", BLOCK + WAIT*abs(n1.x - n2.x))
+    
+    @classmethod
+    def exit_edge(cls, n1, n2):
+        return cls(n1, n2, "EXIT", WAIT)
+
+    @classmethod
+    def use_elevator_edge(cls, n1, n2):
+        return cls(n1, n2, "USE ELEVATOR", WAIT)
+
+    @classmethod
+    def put_elevator_edge(cls, n1, n2):
+        return cls(n1, n2, "PUT ELEVATOR", ELEVATOR)
 
 
-height, width, time, exit_y, exit_x, n_clone, n_elevator, n_starting_elevator = [int(i) for i in input().split()]
-state = State(height, width, time, n_clone, n_elevator, (exit_x, exit_y))
-for i in range(n_starting_elevator):
-    y, x = [int(j) for j in input().split()]
-    state.add_elevator(x, y)
+@dataclass
+class Graph:
+    nodes: MutableSet[Node] = field(default_factory=set)
+    node_properties: DefaultDict[Node, NodeProperties] = field(
+        default_factory=lambda: DefaultDict(NodeProperties)
+    )
+    edges: MutableSet[Edge] = field(default_factory=set)
 
+    def add_node(self, node):
+        self.nodes.add(node)
 
-SCENARIO1 = ELEVATOR + 3*WAIT + 9*WAIT
-SCENARIO2 = ELEVATOR + 3*WAIT + BLOCK
-SCENARIO3 = (ELEVATOR + 3*WAIT)*5
-SCENARIO4 = BLOCK + 9*WAIT + ELEVATOR + 3*WAIT + BLOCK + 6*WAIT + ELEVATOR + 3*WAIT + BLOCK
-SCENARIO5 = 3*WAIT + ELEVATOR + 6*WAIT + ELEVATOR + 3*WAIT + ELEVATOR
-SCENARIO6 = 4*WAIT + BLOCK + 6*WAIT + (4*WAIT + BLOCK)*5
-SCENARIO7 = 4*WAIT + BLOCK + 10*WAIT + ELEVATOR + 4*WAIT + BLOCK + 4*WAIT + BLOCK + 4*WAIT + BLOCK
+    def add_edge(self, edge):
+        self.edges.add(edge)
 
-ACTION_SEQUENCE = list(SCENARIO1)
+    def get_start(self):
+        return next(filter(lambda n: self.node_properties[n].is_start, self.nodes))
 
+    def get_exit(self):
+        return next(filter(lambda n: self.node_properties[n].is_exit, self.nodes))
 
-log(height, width, time, n_clone, n_elevator)
-log(cost(ACTION_SEQUENCE))
-# game loop
-while True:
-    inputs = input().split()
-    y = int(inputs[0])  # floor of the leading clone
-    x = int(inputs[1])  # position of the leading clone on its floor
-    direction = 1 if inputs[2] == 'RIGHT' else -1  # direction of the leading clone: LEFT or RIGHT
-    node = Node(x, y, direction)
+    def get_out_edges(self, node):
+        return filter(lambda e: e.node_from == node, self.edges)
 
-    log(state.simulate(node, ACTION_SEQUENCE))
-    if ACTION_SEQUENCE:
-        action = ACTION_SEQUENCE.pop(0)
-    else:
-        action = "END"
+    def get_in_edges(self, node):
+        return filter(lambda e: e.node_to == node, self.edges)
 
-    log(ACTION_SEQUENCE)
+@dataclass
+class Factory:
+    time: int
+    n_clones: int
+    n_elevators: int
 
+    def add_step_node(self, graph, node):
+        for i in range(0, self.n_elevators):
+            y = node.y - i - 1
+            n = Node((y, node.x, node.direction))
+            graph.add_node(n)
+            graph.node_properties[n].can_put_elevator = True
+            if y != 0:
+                graph.node_properties[n].can_block = True
+                graph.node_properties[n].can_walk = True
+    
+    def add_exit_node(self, graph, y, x):
+        n = Node((y, x, 0), "EXIT")
+        graph.add_node(n)
+        graph.node_properties[n].is_exit = True
 
-    print(action)
+        n1 = Node((y, x, 1), "EXIT PATH")
+        graph.add_node(n1)
+        graph.node_properties[n1].can_exit = True
+        self.add_step_node(graph, n1)
+        n2 = Node((y, x, -1), "EXIT PATH")
+        graph.add_node(n2)
+        graph.node_properties[n2].can_exit = True
+        self.add_step_node(graph, n2)
+
+    def add_start_node(self, graph, y, x):
+        n = Node((y, x, 1), "START")
+        graph.add_node(n)
+        graph.node_properties[n].is_start = True
+        graph.node_properties[n].can_block = True
+        graph.node_properties[n].can_walk = True
+
+    def add_elevator_node(self, graph, y, x, d):
+        n = Node((y, x, d), "ELEVATOR")
+        graph.add_node(n)
+        graph.node_properties[n].is_elevator = True
+        graph.node_properties[n].can_use_elevator = True
+        graph.node_properties[n].can_block = False
+        graph.node_properties[n].can_walk = False
+        graph.node_properties[n].can_put_elevator = False
+
+        n_out = Node((y + 1, x, d))
+        graph.add_node(n_out)
+        if not graph.node_properties[n_out].is_elevator:
+            graph.node_properties[n_out].can_block = True
+            graph.node_properties[n_out].can_walk = True
+
+        self.add_step_node(graph, n)
+        
+
+    def add_edges(self, graph):
+        n_exit = next(filter(lambda n: graph.node_properties[n].is_exit, graph.nodes))
+        for n in filter(lambda n: graph.node_properties[n].can_exit, graph.nodes):
+            edge = Edge.exit_edge(n, n_exit)
+            graph.add_edge(edge)
+
+        for n1 in filter(lambda n: graph.node_properties[n].can_walk, graph.nodes):
+            nodes = filter(lambda n: n1.y == n.y, graph.nodes)
+            nodes = filter(lambda n: n1.x * n1.direction < n.x * n1.direction and n1.direction == n.direction, nodes)
+            for n2 in nodes:
+                edge = Edge.walk_edge(n1, n2)
+                graph.add_edge(edge)
+
+        for n1 in filter(lambda n: graph.node_properties[n].can_block, graph.nodes):
+            nodes = filter(lambda n: n1.y == n.y, graph.nodes)
+            nodes = filter(lambda n: n1.x * n1.direction > n.x * n1.direction and n1.direction != n.direction, nodes)
+            for n2 in nodes:
+                edge = Edge.block_edge(n1, n2)
+                graph.add_edge(edge)
+        
+        for n1 in filter(lambda n: graph.node_properties[n].can_use_elevator, graph.nodes):
+            n2 = next(filter(lambda n: n.position == (n1.y + 1, n1.x, n1.direction), graph.nodes))
+            edge = Edge.use_elevator_edge(n1, n2)
+            graph.add_edge(edge)
+
+        for n1 in filter(lambda n: graph.node_properties[n].can_put_elevator, graph.nodes):
+            n2 = next(filter(lambda n: n.position == (n1.y + 1, n1.x, n1.direction), graph.nodes))
+            edge = Edge.put_elevator_edge(n1, n2)
+            graph.add_edge(edge)
+
+    def parse_grid(self, grid):
+        graph = Graph()
+        for (y, x), value in sorted(np.ndenumerate(grid), reverse=True):
+            if value == 'X':
+                self.add_exit_node(graph, y, x)
+            if value == 'O':
+                self.add_start_node(graph, y, x)
+            if value == '^':
+                self.add_elevator_node(graph, y, x, 1)
+                self.add_elevator_node(graph, y, x, -1)
+        self.add_edges(graph)
+        return graph
+
+@dataclass(frozen=True)
+class Path:
+    node: Node
+    sequence: Tuple[str] = field(default_factory=tuple)
+    nodes: FrozenSet[Node] = field(default_factory=frozenset)
+
+    @property
+    def time(self):
+        return len(self.sequence)
+
+    @property
+    def cost(self):
+        counter = Counter(self.sequence)
+        return self.time, counter['BLOCK'] + counter['ELEVATOR'], counter['ELEVATOR']
+
+    def __gt__(self, other):
+        return self.time > other.time
+
+    def append_edge(self, edge):
+        node = edge.node_to
+        sequence = self.sequence + edge.sequence
+        nodes = self.nodes | set([node])
+        return type(self)(node, sequence, nodes)
+    
+    def get_childs(self, graph):
+        childs = list()
+        for e in graph.get_out_edges(self.node):
+            if e.node_to not in self.nodes:
+                childs.append(self.append_edge(e))
+        return childs
+
+@dataclass
+class Solver:
+    time: int
+    clone: int
+    elevator: int
+
+    @property
+    def cost(self):
+        return self.time, self.clone, self.elevator
+
+    def check_cost(self, path):
+        return all([a <= b for a, b in zip(path.cost, self.cost)])
+
+    def bfs(self, graph, start, end):
+        discovered = set()
+        queue = list()
+        pstart = Path(start)
+        discovered.add(pstart)
+        hq.heappush(queue, (pstart.time, pstart))
+
+        while queue:
+            _, current = hq.heappop(queue)
+            if graph.node_properties[current.node].is_exit:
+                return True, current
+
+            for child in current.get_childs(graph):
+                if (child not in discovered and self.check_cost(child)):
+                    discovered.add(child)
+                    hq.heappush(queue, (child.time, child))
+
+        return False, None
+
+from pprint import pprint
+grid = np.flip(np.array([
+[" ", "X", " ", " ", " "],
+[" ", " ", " ", " ", " "],
+[" ", " ", " ", " ", " "],
+[" ", " ", " ", " ", " "],
+[" ", " ", " ", "O", " "],
+]), axis=0)
+time, clones, elevators = (1000, 20, 2)
+factory = Factory(time, clones, elevators)
+graph = factory.parse_grid(grid)
+print(f"{time=} {clones=} {elevators=}")
+print(f"Nodes {len(graph.nodes)} Edges {len(graph.edges)}")
+
+pprint([g for g in graph.nodes if len(list(graph.get_in_edges(g))) == 0])
+
+solver = Solver(time, clones, elevators)
+start = graph.get_start()
+end = graph.get_exit()
+print(start, end)
+success, path = solver.bfs(graph, start, end)
+
+if success:
+    print("Path found")
+    print(path.node, path.cost, path.time)
+    print(path.sequence)
+    print(path.nodes)
+else:
+    print("Path not found")
